7a5747550dac1ef322b31a2254c4cb2b4c4c0d55
Ribin-Baby <ribinkannoth@gmail.com>
2024-09-11 21:27:01 +0530

    added comments

--- app/app.py
+++ app/app.py
@@ -16,24 +16,28 @@ load_dotenv()
 
 app = FastAPI()
 
-# In-memory database (for demonstration purposes)
+# In-memory database (for future purposes)
 chathistory: List = []
 
-# Here we are using mixtral-8x7b-instruct-v0.1 model from API Catalog
+# Here we are using mixtral-8x7b-instruct-v0.1 model hosted as Nvidia Inference Microservice (NIM)
 Settings.llm = NVIDIA(model="mistralai/mixtral-8x7b-instruct-v0.1", nvidia_api_key=os.environ['NVIDIA_API_KEY'])
+# Here we using NV-Embed-QA embedding model from nvidia
 Settings.embed_model = NVIDIAEmbedding(model="NV-Embed-QA", nvidia_api_key=os.environ['NVIDIA_API_KEY'], truncate="END")
+# document splitting
 Settings.text_splitter = SentenceSplitter(chunk_size=400)
+# reading data from ./data directory and creating documents
 DATA_PATH = f"{os.getcwd()}/data"
 documents = SimpleDirectoryReader(DATA_PATH).load_data()
+# indexing documents to vector DB
 index = VectorStoreIndex.from_documents(documents)
-
+# here we are using nvidiareranker for better retrieval quality.
 reranker_query_engine = index.as_query_engine(
     similarity_top_k=40, node_postprocessors=[NVIDIARerank(top_n=4)]
 )
 
 @app.get("/")
 async def reload():
-    return {"response": "http://localhost/docs -> for how to use !"}
+    return {"response": "http://localhost:8000/docs -> for how to use !"}
 
 # chat here
 @app.get("/query")
